402ee9f213cd9e7d0287377ada085fd714b4e634
Carlos <carlos.grh@outlook.com>
2022-06-05 14:20:02 -0500

    change Bootstrap4 to Bootstrap import

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import smtplib
 from flask import Flask, render_template, request, redirect, send_from_directory, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap4
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired
@@ -44,7 +44,7 @@ organize_cert_path()
 
 app = Flask(__name__)
 
-bootstrap = Bootstrap4(app)
+bootstrap = Bootstrap(app)
 
 app.secret_key = "THIS IS A SECRET KEY $%12&*(VNIJG"
 
