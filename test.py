ca83c5a89aef36705199a9851848be538d783997
Carlos <carlos.grh@outlook.com>
2022-06-07 21:42:17 -0500

    remove test.py file.

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy
-
-dataframe1 = pd.read_excel("professional experiences.xlsx", sheet_name="Sheet2")
-
-print(len(dataframe1))
-
-for n in range(4):
-    print( dataframe1.img[n], dataframe1.cert[n])
